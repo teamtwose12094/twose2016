0b87b2604def2ebe5785a3b356d68d0ee0c3970d
teamtwose12094 <teamtwose12094@gmail.com>
2016-12-08 17:59:35 -0700

    one second forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.HashMap;
+
 /**
  * Created by student on 11/24/2016.
  */
@@ -10,34 +12,19 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous")
 public class Autonomous extends LinearOpMode {
 
-    HardwareK9bot robot = new HardwareK9bot();
-    ElapsedTime timer = new ElapsedTime();
+    private HardwareK9bot robot;
+
+    private int delay;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //Set up the robot hardware
-        //random comment
-
         robot.init(hardwareMap);
 
-        waitForStart();
-
-        //Move forward
         robot.leftMotor.setPower(0.5);
         robot.rightMotor.setPower(0.5);
 
-        timer.reset();
+        sleep(1000);
 
-        //Wait one second
-        while(timer.seconds() < 1 && opModeIsActive()) {
-            sleep(20);
-        }
-
-        //Stop
-        stopMotors();
-    }
-
-    public void stopMotors() {
         robot.leftMotor.setPower(0);
         robot.rightMotor.setPower(0);
     }
