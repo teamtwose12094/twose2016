6978dedf1318cf8ca3a8b9d14f68f5b39aacbd44
teamtwose12094 <teamtwose12094@gmail.com>
2017-01-20 19:58:14 -0700

    added "move" function

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MissionCorner.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by student on 1/20/2017.
+ */
+
+public class MissionCorner extends Autonomous {
+    @Override
+    public void runPath() {
+        move(48, 0.5);
+        pivot(90, 0.5);
+        move(24, 0.5);
+        pivot(45, 0.5);
+        move(48, 0.5);
+    }
+
+}
+
