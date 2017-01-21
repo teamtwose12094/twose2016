23c86c0ad02e045b4ed715aa52d996b537479a9d
teamtwose12094 <teamtwose12094@gmail.com>
2017-01-20 19:59:56 -0700

    Changed MissionCentre to have a much more precise distance.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MissionCentre.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MissionCentre.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by student on 1/20/2017.
  */
-
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous
 public class MissionCentre extends Autonomous {
     @Override
     protected void runPath() {
-
+        move(85,0.5);
     }
 }
