47ce44629099786419b5bbe3315196bdd5dcd00d
Jason <Teamtwose12094@gmail.com>
2017-11-16 18:24:48 -0700

    coloursensortest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColourSensorTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by Instructor on 11/16/2017.
+ */
+
+public abstract class ColourSensorTest extends OpMode {
+
+    ColorSensor color_sensor;
+}
